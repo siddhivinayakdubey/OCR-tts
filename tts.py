28c3bf7b17f6d821926a5f7e615ad37e2744bdcd
siddhivinayakdubey <45829257+siddhivinayakdubey@users.noreply.github.com>
2020-08-05 11:17:57 +0530

    Add files via upload

---
+++ tts.py
@@ -0,0 +1,19 @@
+try:
+  from PIL import Image
+except ImportError:
+  import Image
+import pytesseract
+
+def ocr_core(filename):
+  text=pytesseract.image_to_string(Image.open(filename))
+  return text
+
+mytext=ocr_core('oculli.jpg')
+print(mytext)
+
+
+from gtts import gTTS
+import os
+tts = gTTS(text=mytext, lang='en')
+tts.save("mytext.mp3")
+os.system("mpg321 mytext.mp3")
